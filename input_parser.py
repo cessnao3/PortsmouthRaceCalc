00a38e1381e02272b245036bc0c804cde23ab37a
Ian O'Rourke <cessna.ian@gmail.com>
2019-08-31 22:28:46 -0500

    Modified for input file folder

--- input_parser.py
+++ input_parser.py
@@ -7,10 +7,11 @@ import skipper_database as sdb
 
 import yaml
 
-with open('series.yaml', 'r') as f:
+
+with open(data_file('series.yaml'), 'r') as f:
     series_data = yaml.safe_load(f)
 
-with open('fleets.yaml', 'r') as f:
+with open(data_file('fleets.yaml'), 'r') as f:
     fleet_data = yaml.safe_load(f)
 
 fleets = dict()
@@ -18,11 +19,11 @@ fleets = dict()
 for f in fleet_data:
     fleet_dict = fleet_data[f]
 
-    with open(fleet_dict['portsmouth_table'], 'r') as f_handle:
+    with open(data_file(fleet_dict['portsmouth_table']), 'r') as f_handle:
         boat_table = f_handle.read()
     fleets[f] = bdb.Fleet(name=f, boat_types=bdb.BoatType.load_from_csv(boat_table))
 
-with open('skippers.csv', 'r') as f:
+with open(data_file('skippers.csv'), 'r') as f:
     skippers = sdb.Skipper.load_from_csv(f.read())
 
 series = dict()
